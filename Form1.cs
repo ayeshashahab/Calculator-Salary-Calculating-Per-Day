468fdae2b236ce8c060ab58f2ae15279dbda4f1a
Ayesha Akram shahab <108744690+ayeshashahab@users.noreply.github.com>
2023-09-22 18:39:02 +0500

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my_first_from
+{
+    public partial class Form1 : Form
+        
+    {
+        int days, spel, total;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            days = Convert.ToInt32(textBox1.Text);
+            spel = Convert.ToInt32(textBox2.Text);
+            total = days * spel;
+            MessageBox.Show(total.ToString());
+        }
+    }
+}
